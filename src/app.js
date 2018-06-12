d659bcc45dff6a18cf6dda1e904c2d94f2518bd5
Sam Xie <samxie@outlook.com.au>
2018-06-12 14:48:51 +1000

    initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+import Header from './components/Header';
+
+ReactDOM.render(<h1>Sam Xie</h1>, document.getElementById("app"));
