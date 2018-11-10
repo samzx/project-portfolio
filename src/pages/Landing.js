1ff37014ae2063890d9dfa4afd2feef0f08f40d6
Sam Xie <samxie@outlook.com.au>
2018-11-10 23:59:51 +1100

    update links order

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -12,13 +12,13 @@ const Landing = () => {
         <h2>Projects</h2>
         <a href="/projects">Go to Projects 👉</a>
       </div>
-      <h2 style={{textAlign: "center"}}>Follow</h2>
-      {
-        follow.map(link => <div style={{textAlign: "center", margin: "1rem"}} key={link.name}><a href={link.src} target="blank">{link.name}</a></div>)
-      }
       <h2 style={{textAlign: "center"}}>Profile</h2>
       {
         profile.map(link => <div style={{textAlign: "center", margin: "1rem"}} key={link.name}><a href={link.src} target="blank">{link.name}</a></div>)
+      }
+      <h2 style={{textAlign: "center"}}>Follow</h2>
+      {
+        follow.map(link => <div style={{textAlign: "center", margin: "1rem"}} key={link.name}><a href={link.src} target="blank">{link.name}</a></div>)
       }
     </div>
   );
