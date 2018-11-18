126335adfd9ef4bc8264a41c794f8a16a1f74b0f
Sam Xie <samxie@outlook.com.au>
2018-11-18 21:57:45 +1100

    added more projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,6 +6,14 @@ export const developments = [
     description: "An upcoming virtual reality game. With a unique locomotion system, blaze through dangerous environments."
   },
   {
+    name: "Good Old Mates",
+    src:
+      "https://res.cloudinary.com/xielabs/image/upload/v1541582261/goodoldmates.png",
+    link: "",
+    description:
+      "Good Old Mates aims to empower elderly users to connect with each other by simplifying their user experience."
+  },
+  {
     name: "MPHWatch",
     src:
       "https://res.cloudinary.com/xielabs/image/upload/v1541582260/mphwatch.png",
@@ -27,5 +35,11 @@ export const developments = [
       "https://res.cloudinary.com/xielabs/image/upload/v1541582252/zendesk_mobile_ticket_viewer.png",
     link: "https://github.com/samzx/zendesk-coding-challenge",
     description: "A mobile ticket viewer for Zendesk's coding challenge."
+  },
+  {
+    name: "OpenGL Art",
+    src: "https://camo.githubusercontent.com/58d58e7ebfd73bb95d07c5221b84e217270e2d32/68747470733a2f2f7468756d62732e6766796361742e636f6d2f4461726c696e674a6f7966756c416d65726963616e726f62696e2d73697a655f726573747269637465642e676966",
+    link: "https://github.com/samzx/opengl2d-drawing",
+    description: "Animated artwork made with OpenGL as a study in graphics."
   }
 ];
