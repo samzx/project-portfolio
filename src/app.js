6d6bca0a02482e07b1bc4ebece280e31c6145a54
Sam Xie <samxie@outlook.com.au>
2018-11-27 20:47:18 +1100

    skeleton modal

--- src/app.js
+++ src/app.js
@@ -8,33 +8,35 @@ import Projects from "./components/Projects";
 import Landing from "./components/Landing";
 import End from "./components/End";
 import Helper from "./components/Helper";
+import About from './components/About'
 
 class App extends React.Component {
   state = {
-    scroll: 0
+    scroll: 0,
+    scrollMax: 0
   };
 
   handleScroll = () => {
+    // TODO: OPTIMISE
     const scrollMax =
       document.body.clientHeight - window.innerHeight;
     const scrollY = window.scrollY;
     const scroll = scrollY / scrollMax;
-    this.setState(() => ({ scroll }));
+    this.setState(() => ({ scroll, scrollMax }));
   };
 
   componentDidMount() {
+    this.handleScroll();
     window.addEventListener("scroll", this.handleScroll);
   }
 
   render() {
-    const { scroll } = this.state;
+    const { scroll, scrollMax } = this.state;
     return (
       <div className="app">
-        {
-          // <div className="back">🎲</div>
-        }
+        <About />
         <Landing scroll={scroll} />
-        <Projects scroll={scroll} />
+        <Projects scroll={scroll} scrollMax={scrollMax} />
         <End scroll={scroll} />
         <Helper scroll={scroll} />
       </div>
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+
+const About = () => (
+  <div>
+  </div>
+);
+
+export default About;
